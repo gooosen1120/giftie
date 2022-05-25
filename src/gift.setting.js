1ddce8319c8fd000975b2e7ac7a85d6b26efa67e
gooosen <99778325+gooosen1120@users.noreply.github.com>
2022-05-25 11:09:16 +0800

    Update gift.setting.js

--- src/gift.setting.js
+++ src/gift.setting.js
@@ -6,17 +6,17 @@ export const GLOBAL_KEYS = {
 };
 
 // 是否允许对方在抽奖结束后重新抽奖
-export const IS_SHOW_RESTART = false;
+export const IS_SHOW_RESTART = true;
 
 // 首页配置
 export const homeConfig = {
   // 标题
-  title: '520送礼抽奖',
+  title: '给丫头的送礼抽奖',
   // 留言，数组的每一项代表一行
   messages: [
-    { key: 'a', wording: '这是第一行文案', },
-    { key: 's', wording: '这是第二行文案', },
-    { key: 'd', wording: '然后，这里是第三行文案', },
+    { key: 'a', wording: '希望丫头年年都要得到开心', },
+    { key: 's', wording: '这里会攒够丫头一年之中说喜欢的东西', },
+    { key: 'd', wording: '然后，让丫头在这里能选到自己的幸福', },
   ],
   // 最终解释权归属人
   owner: 'XXX',
@@ -31,12 +31,12 @@ export const homeConfig = {
 // 图片放在 public/images 中，建议是正方形，否则影响视觉体验
 // name 是礼物的全称；alias 是礼物的简称（用来显示在抽奖方格里）
 export const gifts = [
-  { key: 'q', name: 'Dior双飞套装', alias: 'Dior双飞', image: '/images/1.png',  description: '「经典迷人色彩，彰显奢华魅力」' },
-  { key: 'w', name: '腾讯视频终身VIP', alias: 'VIP', image: '/images/2.png',  description: '“为你承包一辈子的 VIP”' },
-  { key: 'e', name: '兰蔻小黑瓶套装', alias: '小黑瓶套装', image: '/images/3.png',  description: '「强维稳，快修护」' },
+  { key: 'q', name: '周大福素指环', alias: '素指环', image: '/images/33.jpeg',  description: '「简单的才是美丽的」' },
+  { key: 'w', name: '该换手机啦', alias: 'iphone14', image: '/images/11.jpeg',  description: '“让手机加速起来”' },
+  { key: 'e', name: '兰蔻小黑瓶套装', alias: '小黑瓶套装', image: '/images/44.jpeg',  description: '「强维稳，快修护」' },
   { key: 'r', name: '520红包', alias: '520', image: '/images/6.png',  description: '“想给你唱一百首情歌”' },
-  { key: 't', name: 'Dior星空套装', alias: 'Dior星空', image: '/images/5.png',  description: '「百变唇妆，精美雕琢」' },
+  { key: 't', name: '迷你洗衣机', alias: '洗衣机', image: '/images/55.jpeg',  description: '「懒丫头能解放一点是一点」' },
   { key: 'y', name: '1314红包', alias: '1314', image: '/images/6.png',  description: '“从今往后，我都会在你旁边”' },
-  { key: 'u', name: 'UR购物券：¥1,000', alias: 'UR', image: '/images/7.png',  description: '“UR！买！”' },
-  { key: 'i', name: 'Dyson美发套装', alias: '戴森', image: '/images/8.png',  description: '「不同造型需求，全面满足」' },
+  { key: 'u', name: '明眸仙女棒', alias: '仙女棒', image: '/images/66.jpeg',  description: '“大眼睛亮起来”' },
+  { key: 'i', name: 'Dyson美发套装', alias: '戴森', image: '/images/22.jpeg',  description: '「不同造型需求，全面满足」' },
 ];
